8d88fe091600aa8299ccce41e1fbf4fb9f2218e9
bakht <haker-1996@mail.ru>
2017-01-28 01:33:43 +0700

    Создание и выполнение простой версии задачи Range Добавлено получение интервала пересечения.

--- Range/src/Testing.java
+++ Range/src/Testing.java
@@ -29,15 +29,43 @@ class Range {
 }
 
 public class Testing {
+    public static double[] getCross(double from1, double to1, double from2, double to2){
+        double[] array = new double[2];
+        if((from2 >= from1 && from2 <= to1) && to2 > to1){
+            array[0] = from1;
+            array[1] = to2;
+            return array;
+
+        }else if((from1 >= from2 && from1 <= to2) && to1 > to2){
+            array[0] = from2;
+            array[1] = to1;
+            return array;
+
+        }else if((from1 >= from2 && to1 <= to2)||(from2 >= from1 && to2 <= to1)){
+            array[0] = from1;
+            array[1] = to1;
+            return array;
+
+        }else {
+            return null;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
 
         Range length = new Range(-6.6, 7.4);
+        Range length1 = new Range(8, 9);
 
         System.out.println("Enter real number:");
         double number = scanner.nextDouble();
 
-        System.out.printf("From: %f To: %f%nInterval is %f%nNumber is in interval: %s", length.getFrom(), length.getTo(), length.getLength(), length.isInside(number));
+        System.out.printf("From: %f To: %f%nInterval is %f%nNumber is in interval: %s%n", length.getFrom(), length.getTo(), length.getLength(), length.isInside(number));
+        System.out.printf("From: %f To: %f%nInterval is %f%nNumber is in interval: %s%n", length1.getFrom(), length1.getTo(), length1.getLength(), length1.isInside(number));
+
+        for ( double x : getCross(length.getFrom(), length.getTo(), length1.getFrom(), length1.getTo())){
+            System.out.print(x + " ");
+        }
     }
 }
