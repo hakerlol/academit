c6fdc97c548ffc8514e750f60c2245ce3ab92e5d
bakht <haker-1996@mail.ru>
2017-02-06 21:26:51 +0700

    Исправлены констукторы для создания Vector. Реализованы нестатические методы сложения, вычитания векторов, умножения вектора на число, разворот вектора и получения длины вектора. Переопределен метод toString.

--- Vector/src/nsk/academit/feller/vector/Vector.java
+++ Vector/src/nsk/academit/feller/vector/Vector.java
@@ -1,48 +1,105 @@
 package nsk.academit.feller.vector;
 
 
+import java.util.Arrays;
+
 public class Vector {
-    private double[] mass;
+    private double[] vectorArray;
 
     public Vector(int size) {
         if (size <= 0) throw new IllegalArgumentException("size can't be negative");
-        double array[] = new double[size];
+        vectorArray = new double[size];
     }
 
     public Vector(Vector vector) {
-        Vector newVector = vector;
+        vectorArray = new double[vector.vectorArray.length];
+        System.arraycopy(vector.vectorArray, 0, this.vectorArray, 0, vector.vectorArray.length);
+
     }
 
     public Vector(double[] array) {
-        mass = new double[array.length];
-        System.arraycopy(array,0, mass, 0, array.length - 1);
+        vectorArray = new double[array.length];
+        System.arraycopy(array, 0, vectorArray, 0, array.length);
     }
 
     public Vector(int size, double[] array) {
-        if (size <= 0) throw new IllegalArgumentException("size can't be negative");
-        mass = new double[array.length];
-        if(array.length < size){
-            for (int i = 0; i < array.length;i++){
-                mass[i] = array[i];
-            }
-            for (int i = array.length; i < size; i++){
-                mass[i] = 0;
-            }
-        } else{
-            for (int i = 0; i < array.length; i++) {
-                mass[i] = array[i];
-            }
+        if (size <= 0) {
+            throw new IllegalArgumentException("size can't be negative");
         }
+        vectorArray = new double[size];
+        System.arraycopy(array, 0, vectorArray, 0, array.length);
+    }
 
+    public Vector getSum(Vector vector) {
+        if (this.vectorArray.length == vector.vectorArray.length) {
+            double[] result = new double[vector.vectorArray.length];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = (this.vectorArray[i] + vector.vectorArray[i]);
+
+            }
+            return new Vector(result);
+        } else {
+            double[] result = new double[this.vectorArray.length > vector.vectorArray.length ? this.vectorArray.length : vector.vectorArray.length];
+            double[] newVectorArray = new double[this.vectorArray.length > vector.vectorArray.length ? this.vectorArray.length : vector.vectorArray.length];
+            System.arraycopy(this.vectorArray.length < vector.vectorArray.length ? this.vectorArray : vector.vectorArray, 0, newVectorArray, 0,
+                    this.vectorArray.length < vector.vectorArray.length ? this.vectorArray.length : vector.vectorArray.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] = (newVectorArray[i] + vector.vectorArray[i]);
+            }
+            return new Vector(result);
+        }
+    }
+
+    public Vector getDifference(Vector vector) {
+        if (this.vectorArray.length == vector.vectorArray.length) {
+            double[] result = new double[vector.vectorArray.length];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = (this.vectorArray[i] - vector.vectorArray[i]);
+
+            }
+            return new Vector(result);
+        } else {
+            double[] result = new double[this.vectorArray.length > vector.vectorArray.length ? this.vectorArray.length : vector.vectorArray.length];
+            double[] newVectorArray = new double[this.vectorArray.length > vector.vectorArray.length ? this.vectorArray.length : vector.vectorArray.length];
+            System.arraycopy(this.vectorArray.length < vector.vectorArray.length ? this.vectorArray : vector.vectorArray, 0, newVectorArray, 0,
+                    this.vectorArray.length < vector.vectorArray.length ? this.vectorArray.length : vector.vectorArray.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] = (newVectorArray[i] - vector.vectorArray[i]);
+            }
+            return new Vector(result);
+        }
+    }
+
+    public Vector calcVectorOnNumber(double number) {
+        for (int i = 0; i < this.vectorArray.length; i++) {
+            this.vectorArray[i] = this.vectorArray[i] * number;
+        }
+        return new Vector(this.vectorArray);
+    }
+
+    public Vector Reverse() {
+        for (int i = 0; i < this.vectorArray.length; i++) {
+            this.vectorArray[i] = this.vectorArray[i] * (-1);
+        }
+        return new Vector(this.vectorArray);
+    }
+
+    public double getVectorLength() {
+        int size = 0;
+        for (int i = 0; i < this.vectorArray.length; i++) {
+            size += Math.pow(vectorArray[i], 2);
+        }
+        return Math.sqrt(size);
     }
 
     public int getSize() {
-        return mass.length;
+        return vectorArray.length;
     }
 
     @Override
     public String toString() {
 
-        return null;
+        return "{" + Arrays.toString(vectorArray) + "}";
     }
+
 }
--- Vector/src/nsk/academit/feller/vector/nsk/academit/feller/vector/main/Main.java
+++ Vector/src/nsk/academit/feller/vector/nsk/academit/feller/vector/main/Main.java
@@ -1,14 +1,24 @@
 package nsk.academit.feller.vector.nsk.academit.feller.vector.main;
 
-
 import nsk.academit.feller.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector(1);
-        double [] array = new double[]{1,2,3,4};
+        Vector vector = new Vector(5);
+        double[] array = new double[]{1, 2, 3, 4};
+        double[] array2 = new double[]{1, 2, 3, 4, 10, 15};
         Vector vector1 = new Vector(array);
-        System.out.println(vector1.getSize());
-        System.out.println(vector);
+        Vector vector2 = new Vector(array2);
+        Vector vector3 = new Vector(10, array);
+        Vector vector4 = new Vector(vector1);
+
+        System.out.println(vector1.getSum(vector2));
+        System.out.println(vector1.getDifference(vector2));
+        System.out.println(vector1.calcVectorOnNumber(5));
+        System.out.println(vector1.Reverse());
+        System.out.println(vector3);
+        System.out.println(vector4);
+        System.out.println(vector4.getSize());
+        System.out.println(vector4.getVectorLength());
     }
 }
