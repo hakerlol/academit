970c38c3a3543fef91b0ba72c50b5bb19fbbb7f6
bakht <haker-1996@mail.ru>
2017-03-25 22:59:34 +0700

    Добавлена проверка на количество аргументов. Оптимизация программы.

--- CSV/src/CSV.java
+++ CSV/src/CSV.java
@@ -12,77 +12,72 @@ public class CSV {
 
         try (Scanner scanner = new Scanner(new FileInputStream(args[0])); PrintWriter writer = new PrintWriter(args[1])) {
 
-            ArrayList<String> arrayList = new ArrayList<>();
-            while (scanner.hasNext()) {
-                arrayList.add(scanner.nextLine());
-            }
+            if (args.length == 2) {
 
-            boolean inQuotes = false;
-            StringBuilder HTMLFile = new StringBuilder();
-            for (String line : arrayList) {
+                ArrayList<String> arrayList = new ArrayList<>();
+                while (scanner.hasNext()) {
+                    arrayList.add(scanner.nextLine());
+                }
 
-                for (int i = 0; i < line.length(); i++) {
-                    char c = line.charAt(i);
+                boolean inQuotes = false;
+                StringBuilder tableHtml = new StringBuilder();
+                for (String line : arrayList) {
+                    if (!inQuotes) {
+                        tableHtml.append("<tr><td>");
+                    }
 
-                    if (c == '<') {
-                        HTMLFile.append("&lt;");
-                    } else if (c == '>') {
-                        HTMLFile.append("&gt;");
-                    } else if (c == '&') {
-                        HTMLFile.append("&amp;");
-                    } else if (inQuotes) {
-                        if (i == line.length() - 1 && c != '"') {
-                            HTMLFile.append(c).append("<br>");
-                        } else if (i == line.length() - 1 && c == '"') {
-                            inQuotes = false;
-                            HTMLFile.append("</td></tr>");
-                        } else if (c != '"') {
-                            HTMLFile.append(c);
-                        } else if (i != line.length() - 1 && line.charAt(i + 1) == '"') {
-                            HTMLFile.append(c);
-                            i += 1;
+                    for (int i = 0; i < line.length(); i++) {
+                        char c = line.charAt(i);
+
+                        if (c == '<') {
+                            tableHtml.append("&lt;");
+                        } else if (c == '>') {
+                            tableHtml.append("&gt;");
+                        } else if (c == '&') {
+                            tableHtml.append("&amp;");
+                        } else if (inQuotes) {
+                            if (i == line.length() - 1 && c != '"') {
+                                tableHtml.append(c).append("<br>");
+                            } else if (c != '"') {
+                                tableHtml.append(c);
+                            } else if (i != line.length() - 1 && line.charAt(i + 1) == '"') {
+                                tableHtml.append(c);
+                                i += 1;
+                            } else {
+                                inQuotes = false;
+                            }
                         } else {
-                            inQuotes = false;
-                            HTMLFile.append("</td>");
-                        }
-                    } else {
-                        if (i == 0 && i == line.length() - 1 && c == ',') {
-                            HTMLFile.append("<tr><td></td></tr>");
-                        } else if (i == 0 && c == ',' && line.charAt(i + 1) != ',' && line.charAt(i + 1) != '"') {
-                            HTMLFile.append("<tr><td></td><td>");
-                        } else if (i == 0 && c == ',') {
-                            HTMLFile.append("<tr><td></td>");
-                        } else if (i == 0) {
-                            HTMLFile.append("<tr><td>").append(c);
-                        } else if (c != ',' && c != '"' && i == line.length() - 1) {
-                            HTMLFile.append(c).append("</td></tr>");
-                        } else if (line.charAt(i - 1) == ',' && c == ',' && line.charAt(i + 1) != ',') {
-                            HTMLFile.append("<td></td><td>");
-                        } else if (c != ',' && c != '"') {
-                            HTMLFile.append(c);
-                        } else if (c == ',' && i == line.length() - 1 && line.charAt(i - 1) == '"') {
-                            HTMLFile.append("<td></td></tr>");
-                        } else if (c == ',' && i == line.length() - 1) {
-                            HTMLFile.append("</td><td></td></tr>");
-                        } else if (c == ',' && line.charAt(i - 1) == '"') {
-                            HTMLFile.append("<td>");
-                        } else if (c == ',' && line.charAt(i - 1) == ',') {
-                            HTMLFile.append("<td></td>");
-                        } else if (c == ',') {
-                            HTMLFile.append("</td><td>");
-                        } else {
-                            inQuotes = true;
+                            if (i == 0 && i == line.length() - 1 && c == ',') {
+                                break;
+                            } else if (i == 0 && c == ',' && line.charAt(i + 1) != ',' && line.charAt(i + 1) != '"') {
+                                tableHtml.append("</td><td>");
+                            } else if (c == ',') {
+                                tableHtml.append("</td><td>");
+                            } else if (i == 0 && c != '"') {
+                                tableHtml.append(c);
+                            } else if (c != '"') {
+                                tableHtml.append(c);
+                            } else {
+                                inQuotes = true;
+                            }
                         }
                     }
+                    if (!inQuotes) {
+                        tableHtml.append("</td></tr>");
+                    }
                 }
+
+                String header = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"/><title>Таблица</title></head><body><table border=\"3\">";
+                String end = "</table></body></html>";
+                writer.print(header);
+                writer.print(tableHtml);
+                writer.print(end);
+
+            } else {
+                System.out.printf("%s%n%s%n%s%n%", "Неверное количество аргументов, проверьте вводимые аргументы",
+                        "Первый аргумент - имя файла в формате .txt, в котором лежит исходная таблица", "Второй аргумент - имя файла в формате .html");
             }
-
-            String header = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"/><title>Таблица</title></head><body><table border=\"3\">";
-            String end = "</table></body></html>";
-            writer.print(header);
-            writer.print(HTMLFile);
-            writer.print(end);
-
+            
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         }
